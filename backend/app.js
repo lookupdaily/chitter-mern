649c03dc950a87df5bf1d648c0db8e3d5d3732a0
Liz Daly <liz7daly@gmail.com>
2020-05-30 19:38:23 +0100

    Add post function

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ class MessageApp {
   constructor() {
     this.messages = []
   }
+
+  post(message) {
+    this.messages.push(message)
+  }
 }
 
 export default MessageApp
--- backend/app.test.js
+++ backend/app.test.js
@@ -2,8 +2,33 @@ import { expect } from 'chai'
 import MessageApp from './app'
 
 describe('app', () => {
-  let testApp = new MessageApp
-  it('app has messages', () => {
-    expect(testApp.messages).to.be.an('array')
+
+  let testApp 
+
+  beforeEach(() => {
+    testApp = new MessageApp
   })
+
+  it('has no messages', () => {
+    expect(testApp.messages.length).to.equal(0)
+  })
+
+  describe('posting a message', () => {
+    beforeEach(() => {
+      testApp.post('hiWorld')
+    })
+
+    it('creates a new message', () => {
+      expect(testApp.messages.length).to.equal(1)
+    })
+  
+    it('messages have date, content and id', () => {
+      expect(testApp.messages).to.include('hiWorld')
+    })
+
+
+  })  
+
+  
+
 })
