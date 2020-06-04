46629a1752b6012c9ebd70b7fc6342c3e7a1dfd0
Liz Daly <liz7daly@gmail.com>
2020-06-04 17:40:49 +0100

    Add textarea element

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import MessageApp from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import { mount } from 'enzyme'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  let component
+
+  beforeEach(() => {
+    component = mount(<MessageApp/>)
+  })
+
+  // it('renders without crashing', () => {
+  //   const component = mount(<MessageApp/>)
+  //   expect(component).toMatchSnapshot()
+  // })
+
+  it('has text box', () => {
+    expect(component.exists('textarea#message_box')).toBe(true)
+  })
+})
